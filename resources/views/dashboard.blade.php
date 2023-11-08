4254ea0eccc927c652543b30e8673296e39c95b9
estov4 <72135548+estov4@users.noreply.github.com>
2023-11-08 22:26:17 +0800

    zsrfb

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -21,11 +21,20 @@
                             </tr>
                             @foreach ($posts as $post)
                                 <tr>
-                                    <th>{{$post->title}}</th>
-                                    <th><a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a></th>
+                                    <td>{{$post->title}}</td>
+                                    <td><a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a></td>
+                                    <td>
+                                        {!!Form::open(['action'=> ['App\Http\Controllers\PostsController@destroy', $post->id, 'method'=>'POST', 'class'=>'pull-right']])!!}
+                                        {{Form::hidden('_method', 'DELETE')}}
+                                        {{Form::submit('Delete', ['class'=>'btn btn-danger'])}}
+                                        {!!Form::close()!!}
+                                    </td>
                                 </tr>
                             @endforeach
                         </table>
+                    
+                    @else
+                        <p> You have no posts</p>
                     @endif
                 </div>
             </div>
