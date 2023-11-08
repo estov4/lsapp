cfc3641d26d29f7a2da038a6285431069f213fc1
estov4 <72135548+estov4@users.noreply.github.com>
2023-11-08 22:20:19 +0800

    dwadasdsaz

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -15,6 +15,6 @@ class Post extends Model
     public $timestamps = true;
 
     public function user(){
-        return $this->belongsTo('App\User');
+        return $this->belongsTo('App\Models\User');
     }
 }
