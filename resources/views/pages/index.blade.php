f2221215003ee7eb818bae974df55d417dcf51c6
Sumyas <143379953+Sumyas@users.noreply.github.com>
2023-11-27 13:52:41 +0800

    Update index.blade.php
    tttttttlll

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -7,7 +7,7 @@
     <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Eventsnes</title>
+	<title>Eventsnes hhhh</title>
 
 	<link rel="stylesheet"  type="text/css" href="css/style.css">
 
